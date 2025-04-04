c647c0227eccc478474d6eb100dc80ce49c8391b
Tomas Rodriguez <federodriguez@me.com>
2025-04-03 21:39:09 -0500

    Modify: Improved Frog input system & architecture
    Improved the way we handle player input by adding a single entry point for input inside the Frog script.
    
    Improved the scripts architecture by decoupling children scripts and calling them with a single parent who orchestrates logic.

--- Assets/Scripts/Frog/Frog.cs
+++ Assets/Scripts/Frog/Frog.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Frog : MonoBehaviour
 {
+    private FrogJump frogJump;
+    private FrogGrounding frogGrounding;
     private Rigidbody2D rb2d;
-
+    private InputSystem_Actions _input;
     void Awake()
     {
+        frogJump = GetComponent<FrogJump>();
+        frogGrounding = GetComponent<FrogGrounding>();
         rb2d = GetComponent<Rigidbody2D>();
+        InputConfiguration();
     }
 
     public Rigidbody2D GetRigidbody2D()
     {
         return rb2d;
     }
+
+    private void InputConfiguration()
+    {
+        _input = new InputSystem_Actions();
+        _input.Player.Enable();
+        _input.Player.Jump.performed += ctx => frogJump.Jump(frogGrounding.IsGrounded());
+    }
+
+
+
 }
